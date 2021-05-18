9d5615ad4734be056312b9c6dec642ec8d29165f
Tina Yuan <71300429+tinay0313@users.noreply.github.com>
2021-05-18 12:42:31 +0800

    Update updatedata.py

--- updatedata.py
+++ updatedata.py
@@ -2,32 +2,20 @@ import pandas as pd
 import re 
 from datetime import datetime, timedelta, date
 
-# df = pd.read_csv("billboardHot100_1999_2019_result.csv")
+df = pd.read_csv("billboardHot100_1999_2019_result.csv")
 
-# df = pd.read_csv("newdata1.csv")
-
-# df['Week']= pd.to_datetime(df['Week'])
-
-# df = df.sort_values(by=['Week','Weekly.rank'],ascending=True)
-
-# df['Explicit'] = df['Explicit'].replace(True, 1).replace(False,0)
-
-# df = df.drop_duplicates(subset=['Week'])
-
-# df = df.drop(columns=['Artist','Date','Genre'])
-
-# df.to_csv("billboard_input.csv", index=False)
-
-
-
-
-
-
-
-
-
-df = pd.read_csv("billboard_input.csv")
 df['Week']= pd.to_datetime(df['Week'])
+
+df = df.sort_values(by=['Week','Weekly.rank'],ascending=True)
+
+#convert string True/False to 1/0
+df['Explicit'] = df['Explicit'].replace(True, 1).replace(False,0)
+
+#keep single top-ranking song for each week
+df = df.drop_duplicates(subset=['Week'])
+
+#remove features that aren't used for model building
+df = df.drop(columns=['Artist','Date','Genre'])
 
 print("Before: ")
 for i in range(df.shape[0]):
@@ -37,24 +25,14 @@ for i in range(df.shape[0]):
 
 for i in range(df.shape[0]):
 	currWeek = df.iloc[i].Week.to_pydatetime().date()
-	# if(currWeek.isoweekday() != 1):
 	offset = currWeek.isoweekday() - 1
 	newDate = currWeek - timedelta(days=offset)
-	# df.iloc[i]['Week'] = newDate
 	df.iloc[i, df.columns.get_loc('Week')] = newDate
 
 print("After: ")
-# print(df.iloc[0]['Week'].date())
 for i in range(df.shape[0]):
-	# currWeek = df.iloc[i].Week.to_pydatetime().date()
 	currWeek = df.iloc[i]['Week']
 	if(currWeek.isoweekday() != 1):
 		print(currWeek)
 
-
 df.to_csv("billboard_input1.csv", index=False)
-
-
-
-
-# print(df)
